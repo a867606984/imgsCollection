04c7c18ab3757b506379dad1f67730e4e554d488
chenping <867606984@qq.com>
2020-08-04 18:09:07 +0800

    18:08ping

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const bodyparser = require('koa-bodyparser');
 const static = require('koa-static');
 const views = require('koa-views');
 const path = require('path');
-const fs = require('fs');
-
+const session = require('koa-session-minimal');
+const MysqlStore = require('koa-mysql-session');
 const Router = require('koa-router');
 const routers = require('./route/index');
+
+//session存储配置
+const sessionMysqlConfig = {
+    user:config.username,
+    password:config.password,
+    database:config.database,
+    host:config.database,
+}
+
+//配置session中间件
+app.use(session({
+    key:'USER_ID',
+    store:new MysqlStore(sessionMysqlConfig)
+}))
 
 //加载post解析
 app.use(bodyparser());
@@ -20,6 +34,8 @@ app.use(static(path.join(__dirname, './static')));
 app.use(views(path.join(__dirname, './views'), {
     extension: 'ejs'
 }))
+
+
 
 //加载路由
 app.use(routers.routes(),routers.allowedMethods());
