fce6bfb01a5a27923fbe078a684572f2e85c5bb9
chenping <867606984@qq.com>
2020-08-05 23:16:10 +0800

    23：16

--- controllers/home.js
+++ controllers/home.js
@@ -2,10 +2,9 @@ const query = require('./util-db');
 
 module.exports = async (env) => {
 
-    let res = await query('SELECT * FROM img_table');
+    let res = await query('SELECT * FROM img_table LIMIT 1,10');
 
     await env.render('index', {
-        title: '这是首页',
         imgList: res
     })
 
